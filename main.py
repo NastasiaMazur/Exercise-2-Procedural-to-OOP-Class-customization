6cde061533aa3afdf69ab3c9a1f423ccdf8a32cc
NastasiaMazur <nvmazour@mail.ru>
2023-11-30 01:04:32 +0100

    Assignment 2: phase 1

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import math
 
+class Point:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}/{self.y})"
+
+    def __repr__(self):
+        return self.__str__()
+
+# Example usage:
+p1 = Point(2.3, 43.14)
+p2 = Point(5.53, 2.5)
+p3 = Point(12.2, 28.7)
+
+print(p1)
+print([p1, p2, p3])
 class Canvas(list):
     def __init__(self, width: int, height: int):
         super().__init__([" " * width for _ in range(height)])
