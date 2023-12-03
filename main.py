506e14df93da096eb14d775c9043a715d58eb2e8
NastasiaMazur <nvmazour@mail.ru>
2023-12-03 19:16:46 +0100

    Assignment2: phase 5

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ print(p6.distance_from_origin())  # Output: 14.142135623730951
 
 # Assignment 2. Phase2
 class Shape(list[Point,...]):   #a shape class inherits from a list of points --> it has all the properties list of points has
+
     def __init__(self, *points):            # this would be inheritance
         self.points = list(points)
 
@@ -56,6 +57,13 @@ class Shape(list[Point,...]):   #a shape class inherits from a list of points --
 
         return Point(avg_x, avg_y)
 
+    def __eq__(self, other):
+        return self.centroid().distance_from_origin() == other.centroid().distance_from_origin()
+
+    def __lt__(self, other):
+        return self.centroid().distance_from_origin() < other.centroid().distance_from_origin()
+
+
 # Example usage:
 s1 = Shape(p1, p2, p3)
 s2 = Shape(p2)
@@ -64,6 +72,14 @@ s3 = Shape()
 s4 = Shape(Point(0, 0), Point(0, 1), Point(1, 1), Point(1, 0))
 s5 = Shape(Point(0, 0.5), Point(0.5, 1), Point(1, 0.5), Point(0.5, 0))
 s6 = Shape(Point(0.25, 0.25), Point(0.25, 0.75), Point(0.75, 0.75), Point(0.75, 0.25))
+
+print(s4 == s5)  # Output: True
+s5 = Shape(Point(5, 5), Point(5, 6), Point(6, 6), Point(6, 5))
+print(s4 < s5)   # Output: True
+s6 = Shape(Point(10, 10), Point(10, 11), Point(11, 11), Point(11, 10))
+shapes = [s6,s4, s5]
+print(shapes)    # Output: [Shape [(10/10), (10/11), (11/11), (11/10)], Shape [(0/0), (0/1), (1/1), (1/0)], Shape [(5/5), (5/6), (6/6), (6/5)]]
+print(sorted(shapes))  # Output: [Shape [(0/0), (0/1), (1/1), (1/0)], Shape [(5/5), (5/6), (6/6), (6/5)], Shape [(10/10), (10/11), (11/11), (11/10)]]
 
 print(s4.centroid())  # Output: (0.5/0.5)
 print(s5.centroid())  # Output: (0.5/0.5)
